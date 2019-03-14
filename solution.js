1fa3684efe9680cab7535679c0a486ee1e4fb438
John Furtado <jf8846@gmail.com>
2019-03-14 18:25:07 -0400

    switched to 'if' statement

--- solution.js
+++ solution.js
@@ -35,43 +35,74 @@ class Person {
 }
 
 class Student extends Person {
+  /*
+   *   Class Constructor
+   *
+   *   @param firstName - A string denoting the Person's first name.
+   *   @param lastName - A string denoting the Person's last name.
+   *   @param id - An integer denoting the Person's ID number.
+   *   @param scores - An array of integers denoting the Person's test scores.
+   */
+  // Write your constructor here
   constructor(firstName, lastName, id, scores) {
     super(firstName, lastName, id);
 
     this.scores = scores;
   }
-
+  /*
+   *   Method Name: calculate
+   *   @return A character denoting the grade.
+   */
+  // Write your method here
   calculate() {
     let sum = 0;
+    let length = this.scores.length;
 
-    for (var i = 0; i < this.scores.length; i++) {
+    for (var i = 0; i < length; i++) {
       sum += this.scores[i];
     }
 
-    let avg = sum / this.scores.length;
+    let avg = sum / length;
 
     let score = "";
 
-    switch (true) {
-      case 90 <= avg <= 100:
-        score = "O";
-        break;
-      case 80 <= avg < 90:
-        score = "E";
-        break;
-      case 70 <= avg < 80:
-        score = "A";
-        break;
-      case 55 <= avg < 70:
-        score = "P";
-        break;
-      case 40 <= avg < 55:
-        score = "D";
-        break;
-      default:
-        score = "T";
-        break;
+    if (90 <= avg <= 100) {
+      score = "O";
+    } else if (80 <= avg < 90) {
+      score = "E";
+    } else if (70 <= avg < 80) {
+      score = "A";
+    } else if (55 <= avg < 70) {
+      score = "P";
+    } else if (40 <= avg < 55) {
+      score = "D";
+    } else {
+      score = "T";
     }
+
+    //     switch (true) {
+    //         case (90 <= avg <= 100):
+    //             score = "O";
+    //             break;
+    //         case (80 <= avg < 90):
+    //             score = "E";
+    //             break;
+    //         case (70 <= avg < 80):
+    //             score = "A";
+    //             break;
+    //         case (55 <= avg < 70):
+    //             score = "P";
+    //             break;
+    //         case (40 <= avg < 55):
+    //             score = "D";
+    //             break;
+    //         case (avg < 40):
+    //             score = "T";
+    //             break;
+    //         default:
+    //             score = "Unable to calculate";
+    //             break;
+    //     }
 
     return score;
   }
