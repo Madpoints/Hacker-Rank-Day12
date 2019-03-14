a3a948b65318b4717b9ead1df25244381ac1c03b
John Furtado <jf8846@gmail.com>
2019-03-14 18:43:51 -0400

    fixed switch statement syntax error, all test cases passed

--- solution.js
+++ solution.js
@@ -13,8 +13,6 @@ function read() {
   return _input[_index++];
 }
 
-/**** Ignore above this line. ****/
-
 class Person {
   constructor(firstName, lastName, identification) {
     this.firstName = firstName;
@@ -34,80 +32,55 @@ class Person {
   }
 }
 
+/// ignore above this line ///
 class Student extends Person {
-  /*
-   *   Class Constructor
-   *
-   *   @param firstName - A string denoting the Person's first name.
-   *   @param lastName - A string denoting the Person's last name.
-   *   @param id - An integer denoting the Person's ID number.
-   *   @param scores - An array of integers denoting the Person's test scores.
-   */
-  // Write your constructor here
   constructor(firstName, lastName, id, scores) {
     super(firstName, lastName, id);
 
     this.scores = scores;
   }
-  /*
-   *   Method Name: calculate
-   *   @return A character denoting the grade.
-   */
-  // Write your method here
+
   calculate() {
     let sum = 0;
+    let avg = 0;
     let length = this.scores.length;
 
     for (var i = 0; i < length; i++) {
-      sum += this.scores[i];
+      sum += parseInt(this.scores[i]);
     }
 
-    let avg = sum / length;
+    avg = sum / length;
 
     let score = "";
 
-    if (90 <= avg <= 100) {
-      score = "O";
-    } else if (80 <= avg < 90) {
-      score = "E";
-    } else if (70 <= avg < 80) {
-      score = "A";
-    } else if (55 <= avg < 70) {
-      score = "P";
-    } else if (40 <= avg < 55) {
-      score = "D";
-    } else {
-      score = "T";
+    switch (true) {
+      case 90 <= avg && avg <= 100:
+        score = "O";
+        break;
+      case 80 <= avg && avg < 90:
+        score = "E";
+        break;
+      case 70 <= avg && avg < 80:
+        score = "A";
+        break;
+      case 55 <= avg && avg < 70:
+        score = "P";
+        break;
+      case 40 <= avg && avg < 55:
+        score = "D";
+        break;
+      case avg < 40:
+        score = "T";
+        break;
+      default:
+        score = "Unable to calculate";
+        break;
     }
-
-    //     switch (true) {
-    //         case (90 <= avg <= 100):
-    //             score = "O";
-    //             break;
-    //         case (80 <= avg < 90):
-    //             score = "E";
-    //             break;
-    //         case (70 <= avg < 80):
-    //             score = "A";
-    //             break;
-    //         case (55 <= avg < 70):
-    //             score = "P";
-    //             break;
-    //         case (40 <= avg < 55):
-    //             score = "D";
-    //             break;
-    //         case (avg < 40):
-    //             score = "T";
-    //             break;
-    //         default:
-    //             score = "Unable to calculate";
-    //             break;
-    //     }
 
     return score;
   }
 }
-
+/// ignore below this line ///
 function main() {
   let firstName = read();
   let lastName = read();
